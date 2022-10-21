2e95d25519c933b412aff9f647e64bf69a9da579
VortX87 <paul_jewkes@hotmail.co.uk>
2022-10-21 10:38:38 +0100

    Fetch article details

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import apiURL from '../api'
 
 export const Page = ({ page }) => {
 
+  const [pageDetails, setPageDetails] = useState({})
+
+  async function fetchDetails() {
+    try {
+      const response = await fetch(`${apiURL}/wiki/${page.slug}`)
+      const pageData = await response.json()
+      setPageDetails(pageData)
+      console.log(pageData)
+    } catch (err) {
+      console.log("Oh no an error!", err)
+    }
+  }
+
+  useEffect(() => {
+    fetchDetails()
+  }, [])
 
 
   return <>
